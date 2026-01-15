165583c75729b83204df24f2e66e23e83ab0780b
Mustafa Furkan ŞAHİN <mfurkansahn@hotmail.com>
2026-01-15 09:57:48 +0300

    Implement the complete and exit buttons

--- gui.py
+++ gui.py
@@ -2,20 +2,30 @@ import functions
 import FreeSimpleGUI as GUI
 
 label = GUI.Text("Type in to-do")
+
 input_box = GUI.InputText(tooltip="Enter a todo", key='todo')
 add_button = GUI.Button("Add")
+
 list_box = GUI.Listbox(values=[todo.strip() for todo in functions.get_todos()], key='todos',
                       enable_events=True, size=(45,10))
 edit_button = GUI.Button("Edit")
+complete_button = GUI.Button("Complete")
+
+exit_button = GUI.Button("Exit")
 
 window = GUI.Window('My ToDo App',
-                    layout=[[label],[input_box, add_button], [list_box, edit_button]],
+                    layout=[[label],
+                            [input_box, add_button],
+                            [list_box, edit_button, complete_button],
+                            [exit_button]],
                     font=("Helvetica", 20))
+
 while True:
     event, values = window.read()
     print(1, event)
     print(2, values)
     print(3, values['todos'])
+
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -24,22 +34,34 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(
                 values=[todo.strip() for todo in todos])
+
         case "Edit":
             if not values['todos']:
                 GUI.popup("Please select a task to edit")
                 continue
 
             todo_to_edit = values['todos'][0]
-            new_todo = values['todo'] + "\n"
+            new_todo = values['todo'].strip() + "\n"
 
             todos = functions.get_todos()
-            index = todos.index(todo_to_edit + "\n")
+            index = todos.index(todo_to_edit.strip() + "\n")
             todos[index] = new_todo
 
             functions.write_todos(todos)
             window['todos'].update(
                 values=[todo.strip() for todo in todos]
             )
+
+        case 'Complete':
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete + "\n")
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+
+        case "Exit":
+            break
 
         case 'todos':
             window['todo'].update(value=values['todos'][0])
